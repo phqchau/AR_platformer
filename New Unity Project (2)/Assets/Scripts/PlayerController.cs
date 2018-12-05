c5b9d1bce2d5f0f709ee9e1e9326552edc816a61
BenPaff <bcpaff16@earlham.edu>
2018-12-04 22:07:54 -0500

    Unity Project
    Update contains full unity project

---
+++ New Unity Project (2)/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpForce;
+    public CharacterController controller;
+
+    private Vector3 moveDirection;
+    public float gravityScale;
+    private int frameCountSince;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveDirection = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal") * moveSpeed, moveDirection.y, CrossPlatformInputManager.GetAxis("Vertical") * moveSpeed);
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            if (controller.isGrounded)
+            {
+                moveDirection.y = jumpForce;
+            }
+        }
+
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+        controller.Move(moveDirection * Time.deltaTime);
+
+	if (gameObject.transform.position.y < -10)
+	{
+		SceneManager.LoadScene("LoseScreen");
+	}
+	}
+}
